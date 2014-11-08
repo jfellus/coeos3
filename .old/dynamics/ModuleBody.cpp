7ad68f46479f143a26c2f2be1d8508e4a14a220b
jfellus <jfellus@gmail.com>
2014-11-08 02:55:48 +0100

    ok

--- .old/dynamics/ModuleBody.cpp
+++
@@ -1,8 +0,0 @@
-/*
- * ModuleBody.cpp
- *
- *  Created on: 29 oct. 2014
- *      Author: jfellus
- */
-
-#include "ModuleBody.h"
--- .old/widget/PropertiesForm.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * PropertiesForm.cpp
- *
- *  Created on: 9 oct. 2014
- *      Author: jfellus
- */
-
-#include "PropertiesForm.h"
-
